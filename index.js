97b883b55e5e6bf68a8b154c7c5c480d1cbccabc
David Reid <david@admiredigital.com>
2021-10-26 18:42:02 +0100

    Adds ball example

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 let gameloop = new GameLoop();
+let ball = new Ball();
 
 gameloop.init = function() {
-    console.log('Todo, init game objects.');
+    ball.init(gameloop.cnv);
 }
 
 gameloop.update = function() {
-    console.log('Todo, update game objects');
+    ball.update(gameloop.cnv);
 }
 
 gameloop.render = function() {
-    console.log('Todo, render game objects');
+    gameloop.ctx.fillStyle = '#1bafdb';         
+    gameloop.ctx.fillRect(0,0, gameloop.cnv.width, gameloop.cnv.height);
+    ball.render(gameloop.ctx);
 }
 
 window.onload = function() {
